accf2bf7503a8f579cf5dca78d3a54ccc7cab417
Luke C <52179123+lukeac2@users.noreply.github.com>
2023-05-05 17:41:58 -0500

    inital commit after fork and other updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css';
-import ModalPopup from './ModalPopup';
-import { useState } from 'react';
-import axios from 'axios';
+import "./App.css";
+import ModalPopup from "./ModalPopup";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
   const [image, setImage] = useState('');
   const [savedImages, setSavedImages] = useState(items)
 
-  const baseURL = 'http://localhost:5500'
+  const baseURL = "http://localhost:5500"
 
   const handleGenerate = async () => {
     setPopup(true)
@@ -38,7 +38,7 @@ function App() {
         <input type="text" placeholder="Type descriptive prompt"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)} />
-        <button className='generate-btn' onClick={handleGenerate}>Genarate</button>
+        <button className="generate-btn" onClick={handleGenerate}>Genarate</button>
       </div>
       <h4>Recently saved Images</h4>
       <div className="imageGallery">
--- server/index.js
+++ server/index.js
@@ -1,40 +1,40 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const { Configuration, OpenAIApi } = require("openai");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const { API_KEY } = process.env
+const { API_KEY } = process.env;
 
 const configuration = new Configuration({
-    apiKey: API_KEY,
+  apiKey: API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-app.get('/', (req, res)=>{
-    res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-
-app.post('/generate', async (req, res) => {
-    const prompt = req.body.prompt
-    console.log(prompt)
-    try {
-        const response = await openai.createImage({
-            prompt,
-            n: 1,
-            size: "512x512",
-        });
-        res.send(response.data.data[0].url);
-    } catch (err) {
-        res.send(err.message);
-    }
-})
-
+app.post("/generate", async (req, res) => {
+  console.log(API_KEY);
+  const prompt = req.body.prompt;
+  console.log(prompt);
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "512x512",
+    });
+    console.log(response.data.data[0].url);
+    res.send(response.data.data[0].url);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 
 // app.post('/users', (req, res) => {
 //     const user = {
@@ -45,5 +45,5 @@ app.post('/generate', async (req, res) => {
 //   })
 
 app.listen(5500, () => {
-    console.log("server started");
+  console.log("server started");
 });
